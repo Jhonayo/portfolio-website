9dd85b5b3164855fd311e5ae0df3cb86409ea12f
Jhonayo <j.iramirezpavez@gmail.com>
2025-04-07 05:35:00 -0400

    WIP: avance home, aboutme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,33 @@
 import { useEffect } from "react"
 
-export const Navbar = ({menuOpen, setMenuOpen}) => {
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
 
   useEffect(() => {
-    document.body.style.overflow =menuOpen ? "hidden" : "";
-  },[menuOpen]);
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
 
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <nav className="top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="w-full px-6 md:max-w-5xl md:mx-auto">
         <div className="flex justify-between items-center h-16">
           <a href="#" className="font-mono text-xl font-bold text-white">
             juan <span className="text-blue-500"> test.page </span></a>
 
-            <div 
-            className="flex items-center justify-center w-10 h-10 relative cursor-pointer z-40 md:hidden" 
+          <div
+            className="flex items-center justify-center w-10 h-10 relative cursor-pointer z-40 md:hidden"
             onClick={() => setMenuOpen((prev) => !prev)}
           >
-            <span className="text-2xl text-white">&#9776;</span>
+            <span className="text-2xl text-white">
+              {menuOpen ?
+                <span class="material-symbols-outlined">
+                  close
+                </span>
+                :
+                <span class="material-symbols-outlined">
+                  menu
+                </span>
+              }
+            </span>
           </div>
           <div className="hidden md:flex items-center space-x-8">
             <a href="#home" className="text-gray-300 hover:text-white transition-colors">
@@ -31,7 +41,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             </a>
             <a href="#contact" className="text-gray-300 hover:text-white transition-colors">
               {" "} Contact {" "}
-            </a>     
+            </a>
           </div>
         </div>
       </div>
